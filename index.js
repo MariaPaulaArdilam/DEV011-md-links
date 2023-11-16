5439def637db92e9f9381005a157f7e5c44cb0ea
MariaPaulaArdilam <antonietainfo@gmail.com>
2023-11-16 15:00:26 -0500

    la ruta es absoluta y existe

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-module.exports = () => {
-  // ...
+//importar los paquetes
+const fs = require("fs");
+const path = require("path");
+const filePath = "README.md";
+
+const mdLinks = (filePath) => {
+  return new Promise((resolve, reject) => {
+    // convertir la ruta en absoluta
+    const absoluteFilePath = path.resolve(filePath);
+    // comprobar que la ruta existe
+    if (!fs.existsSync(absoluteFilePath)) {
+      reject(new Error("La ruta no existe"));
+    } else {
+      // comprobar que el archivo existe
+      resolve(`La ruta ${absoluteFilePath} existe.`);
+    }
+  });
 };
+
+module.exports = mdLinks;
+
+mdLinks(filePath)
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
+
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,13 @@
-const mdLinks = require('../');
+const mdLinks = require('../index.js');
 
 
 describe('mdLinks', () => {
 
   it('should...', () => {
     console.log('FIX ME!');
+  });
+  it('Debe devolver una promesa', () => {
+    expect(mdLinks()).toBeInstanceOf(Promise);
   });
 
 });
