56bfc8ced7e90125d109629a37defa4e88e29a4e
MariaPaulaArdilam <antonietainfo@gmail.com>
2023-11-30 16:08:34 -0500

    --stats

--- cli.js
+++ cli.js
@@ -1,18 +1,17 @@
 #! /usr/bin/env node
-
 const mdLinks = require("./src/index.js");
-let filePath = process.argv[2]  //"READMEPRUEBA.md"
-let validate = true
-let stats = true 
+let filePath = process.argv[2];
+let validate = false;
+let stats = false;
 
 const args = process.argv.slice(2);
 
 if (args.length !== 1 && args.length !== 2 && args.length !== 3) {
-  console.log('Uso: md-links READMEDEPRUEBA.md');
+  console.log('Uso: md-links READMEDEPRUEBA.md [--validate] [--stats]');
 } else {
   filePath = args[0];
-  validate = args[1] === '--validate';
-  stats = args[2] === '--stats';
+  validate = args.includes('--validate');
+  stats = args.includes('--stats');
 }
 
 mdLinks(filePath, validate, stats)
@@ -20,5 +19,3 @@ mdLinks(filePath, validate, stats)
     console.log(res);
   })
   .catch((err) => console.log(err));
-
-
--- src/index.js
+++ src/index.js
@@ -3,41 +3,48 @@ const { absolutePath, validar, obtenerEnlacesMarkdown, validateLinks } = require
 
 const mdLinks = (path, validate, stats) => {
   return new Promise((resolve, reject) => {
-    // convertir la ruta en absoluta
     const absoluteFilePath = absolutePath(path);
-    // comprobar que la ruta existe
-    if (!validar(absoluteFilePath)) {
 
+    if (!validar(absoluteFilePath)) {
       reject(new Error("La ruta no existe"));
     } else {
       obtenerEnlacesMarkdown(absoluteFilePath)
-      .then((links) => {
-        if (validate) {
-          const linkPromises = links.map(linkObj => {
-            return validateLinks(linkObj.href); // Validar cada enlace    
-            })
-        Promise.all(linkPromises)
-        .then(validatedLinks => {
-          // Combinar información de enlaces validados con información original
-          const combinedLinks = links.map((linkObj, index) => {
-            return { ...linkObj, ...validatedLinks[index] };
-          });
-          // console.log(combinedLinks, 9876);
-          resolve(combinedLinks);
+        .then((links) => {
+          if (stats) {
+            const totalLinks = links.length;
+            const uniqueLinks = [...new Set(links.map((link) => link.href))].length;
+
+            const statistics = {
+              total: totalLinks,
+              unique: uniqueLinks,
+              // Agrega más estadísticas según sea necesario
+            };
+
+            resolve(statistics);
+          } else if (validate) {
+            const linkPromises = links.map((linkObj) => {
+              return validateLinks(linkObj.href); // Validar cada enlace
+            });
+
+            Promise.all(linkPromises)
+              .then((validatedLinks) => {
+                const combinedLinks = links.map((linkObj, index) => {
+                  return { ...linkObj, ...validatedLinks[index] };
+                });
+                resolve(combinedLinks);
+              })
+              .catch((error) => {
+                reject(error);
+              });
+          } else {
+            resolve(links);
+          }
         })
-        .catch(error => {
+        .catch((error) => {
           reject(error);
         });
-  } else {
-    // console.log('LINKS: ', links);
-    resolve(links);
-  }
-})
-.catch((error) => {
-  reject(error);
-});
-  }
+    }
   });
-}
+};
 
 module.exports = mdLinks;
